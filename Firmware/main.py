d59e634d51d8a3e3ec1caed0d1ea38f1958467c4
shreeyuvan2020 <63835553+shreeyuvan2020@users.noreply.github.com>
2025-06-09 17:21:13 +0530

    Update main.py

--- Firmware/main.py
+++ Firmware/main.py
@@ -4,6 +4,7 @@ from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
 from kmk.modules.macros import Press, Release, Tap, Macros
 from kmk.modules.encoder import EncoderHandler
+from kmk.modules.rgb import RGB
 
 keyboard = KMKKeyboard()
 
@@ -12,8 +13,12 @@ keyboard.modules.append(macros)
 encoder_handler = EncoderHandler()
 keyboard.modules.append(encoder_handler)
 
-PINS = [board.D3, board.D4, board.D2, board.D1]
-ENCODER_PINS = (board.D5, board.D6)
+# Initialize RGB module
+rgb = RGB(pixel_pin=board.D7, num_pixels=2)
+keyboard.modules.append(rgb)
+
+PINS = [board.D5, board.D10, board.D11, board.D12]
+ENCODER_PINS = (board.D9, board.D8)
 
 keyboard.matrix = KeysScanner(
     pins=PINS,
@@ -25,22 +30,26 @@ encoder_handler.pins = [ENCODER_PINS]
 encoder_handler.map = [((KC.VOLU, KC.VOLD),)]
 
 sleep_restart = KC.MACRO(
-    on_press=[Press(KC.LCTL), Press(KC.LCMD), Tap(KC.PWR), Release(KC.LCMD), Release(KC.LCTL)],
-    on_hold=[Press(KC.LCTL), Press(KC.LCMD), Press(KC.PWR), Release(KC.PWR), Release(KC.LCMD), Release(KC.LCTL)],
+    on_press=[Press(KC.LCMD), Press(KC.LSHIFT), Press(KC.OPTION), Press(KC.Q), Release(KC.LCMD), Release(KC.LSHIFT), Release(KC.OPTION)],
+    on_hold=[Press(KC.LCMD), Press(KC.LSHIFT), Tap(KC.Q), Release(KC.LSHIFT), Release(KC.LCMD)],
     hold_time=500
 )
 
 minimize_all = KC.MACRO(Press(KC.LCMD), Press(KC.LALT), Tap(KC.M), Release(KC.LALT), Release(KC.LCMD))
 
 open_slack = KC.MACRO(
-    Press(KC.LCMD), Tap(KC.T), Release(KC.LCMD),
-    Tap(KC.S), Tap(KC.L), Tap(KC.A), Tap(KC.C), Tap(KC.K), Tap(KC.ENTER)
+    Press(KC.LCMD), Press(KC.LSHIFT), Tap(KC.S), Release(KC.LSHIFT), Release(KC.LCMD)
 )
 
 open_vscode = KC.MACRO(
-    Press(KC.LCMD), Tap(KC.T), Release(KC.LCMD),
-    Tap(KC.V), Tap(KC.S), Tap(KC.SPACE), Tap(KC.C), Tap(KC.O), Tap(KC.D), Tap(KC.E), Tap(KC.ENTER)
+    Press(KC.LCMD), Press(KC.LSHIFT), Tap(KC.S), Release(KC.LSHIFT), Release(KC.LCMD)
 )
+
+# Set Neopixel colors
+rgb.colors = [
+    (0, 0, 255),  # Blue
+    (255, 255, 0)  # Yellow
+]
 
 keyboard.keymap = [
     [sleep_restart, minimize_all, KC.NO, open_slack, open_vscode]
